a01565a432cfc6b6396a6c67d87120239070d7e3
Smitbhuva15 <itssmit123@gmail.com>
2025-08-01 05:39:37 +0000

    Donate ETH to a campaign

--- web3/contracts/ Campaign.sol
+++ web3/contracts/ Campaign.sol
@@ -28,6 +28,11 @@ contract Campaign {
 
     error goalmustBeGreaterThanZero();
     error durationmustBeGreaterThanZero();
+    error  CampaignNotFound();
+    error paymustBeGreaterThanZero();
+    error sendermustBeValid();
+    error campaignIsNotActive();
+    error timeStampExceeded();
 
     /////////////////////////         events      //////////////////////////////
 
@@ -44,6 +49,8 @@ contract Campaign {
         bool active
     );
 
+    event Donate(uint256 id, address donor, uint256 value);
+
     function createCampaign(
         string memory title,
         string memory description,
@@ -51,7 +58,6 @@ contract Campaign {
         uint256 goal,
         uint256 durationinDay
     ) public {
-   
         if (goal <= 0) {
             revert goalmustBeGreaterThanZero();
         }
@@ -86,5 +92,36 @@ contract Campaign {
             false,
             true
         );
+    }
+
+    // Donate ETH to a campaign
+    function donate(uint256 _id) public payable {
+         
+         if(_id<=0 && _id>CampaignCount){
+            revert CampaignNotFound(); 
+         }
+
+         if(msg.value<=0){
+            revert paymustBeGreaterThanZero();
+         }
+
+         if(msg.sender == address(0)){
+            revert sendermustBeValid();
+         }
+
+         if(campaigns[_id].active == false){
+            revert campaignIsNotActive();
+         }
+
+         if(block.timestamp > campaigns[_id].deadline){
+            revert timeStampExceeded();
+         }
+
+        Campaigns storage c = campaigns[_id];
+
+        c.raised = c.raised + msg.value;
+        contributions[_id][msg.sender] += msg.value;
+
+        emit Donate(c.id, msg.sender, msg.value);
     }
 }
