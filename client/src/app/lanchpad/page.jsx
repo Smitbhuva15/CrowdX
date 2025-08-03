c2f107fb9b2b46923853ce4c8e75405179084085
Smitbhuva15 <itssmit123@gmail.com>
2025-08-03 08:01:40 +0000

    rearrange files

---
+++ client/src/app/lanchpad/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const page = () => {
+  return (
+     <div className="text-white p-6">
+      <h1 className="text-2xl font-bold">Launchpad</h1>
+      <p>Welcome to the Launchpad section.</p>
+    </div>
+  )
+}
+
+export default page
